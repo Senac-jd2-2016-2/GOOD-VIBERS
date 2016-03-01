8b22db4136e50740117076cb9a2bf18e08c45a84
adrieltails <adrielbrenner@hotmail.com>
2016-03-01 07:17:47 -0300

    commit inicial

---
+++ Game2/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game2
+{
+  
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+  
+        protected override void Initialize()
+        {
+   
+
+            base.Initialize();
+          
+        }
+
+  
+        protected override void LoadContent()
+        {
+            Jogador j1 = new Jogador();
+            j1.setTextura(Content.Load<Texture2D>("Kirby"));
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+          
+        }
+
+  
+        protected override void UnloadContent()
+        {
+        }
+
+       
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+      
+
+            base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            SpriteBatch.Begin();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Game2/Personagem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game2
+{
+    class Personagem
+    {
+        private float posicaoX;
+        private float posicaoY;
+        private Texture2D textura;
+        private bool estaVivo;
+
+        //Encapsulamento
+        public float getPosicaoX()
+        {
+            return posicaoX;
+        }
+        public void setPosicaoX(float posicaoX)
+        {
+            this.posicaoX = posicaoX;
+        }
+        public float getPosicaoY()
+        {
+            return posicaoY;
+        }
+        public void setPosicaoY(float posicaoY)
+        {
+            this.posicaoY = posicaoY;
+        }
+        public Texture2D getTextura()
+        {
+            return textura;
+        }
+        public void setTextura(Texture2D textura)
+        {
+            this.textura = textura;
+        }
+
+        public bool getEstaVivo()
+        {
+            return estaVivo;
+        }
+        public void setEstaVivo(bool estaVivo)
+        {
+            this.estaVivo = estaVivo;
+        }
+
+
+    
+    }
+}
